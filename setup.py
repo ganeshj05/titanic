ca6efd5e4c860dd49f15148b6f6a7a260c2d4b22
ganeshj05 <ganesh.j0509@gmail.com>
2018-07-14 21:19:33 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='FirstTryforDatascience project[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C',
+    author='GaneshJadhav',
+    license='MIT',
+)
